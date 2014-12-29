f474aba605e96e1032f000a7263878cf6975c957
Todd Rogers <todd@windfox.net>
2014-12-28 21:08:40 -0500

    basic layout of a weekly calendar done.

--- app/Controller/MealPlansController.php
+++ app/Controller/MealPlansController.php
@@ -8,18 +8,24 @@ App::uses('AppController', 'Controller');
  */
 class MealPlansController extends AppController {
 
-    public $components = array('Paginator');
-
     /**
      * index method
      *
      * @return void
      */
-    public function index() {
-        //$this->MealPlan->recursive = 0;
+    public function index($date = null) {
         $weekDays = $this->MealPlan->DaysFull;
-        $this->set(compact('weekDays'));
-        //$this->set('mealPlans', $this->Paginator->paginate());
+        $this->MealPlan->InitDate($date);
+        $weekList = $this->MealPlan->getWeekDaysList();
+        $currentMonth = $this->MealPlan->currentMonth;
+        $startDate = $this->MealPlan->MonthsAbbreviated[$weekList[0][1]-1] . " " . $weekList[0][0] . ", " . $weekList[0][2];
+        $endDate = $this->MealPlan->MonthsAbbreviated[$weekList[6][1]-1] . " " . $weekList[6][0] . ", " . $weekList[6][2];
+        $nextWeek = $this->MealPlan->getNextWeek();
+	$previousWeek = $this->MealPlan->getPreviousWeek();
+        $realDay = $this->MealPlan->realDay;
+        $realMonth = $this->MealPlan->realMonth;
+        $realYear = $this->MealPlan->realYear;
+        $this->set(compact('weekDays', 'weekList', 'currentMonth', 'startDate', 'endDate', 'nextWeek', 'previousWeek', 'realDay', 'realMonth', 'realYear'));
     }
 
     /**
@@ -30,24 +36,24 @@ class MealPlansController extends AppController {
      * @return void
      */
     public function edit($id = null) {
-            if (!$this->MealPlan->exists($id)) {
-                    throw new NotFoundException(__('Invalid meal plan'));
-            }
-            if ($this->request->is(array('post', 'put'))) {
-                    if ($this->MealPlan->save($this->request->data)) {
-                            $this->Session->setFlash(__('The meal plan has been saved.'));
-                            return $this->redirect(array('action' => 'index'));
-                    } else {
-                            $this->Session->setFlash(__('The meal plan could not be saved. Please, try again.'));
-                    }
-            } else {
-                    $options = array('conditions' => array('MealPlan.' . $this->MealPlan->primaryKey => $id));
-                    $this->request->data = $this->MealPlan->find('first', $options);
-            }
-            $mealNames = $this->MealPlan->MealName->find('list');
-            $recipes = $this->MealPlan->Recipe->find('list');
-            $users = $this->MealPlan->User->find('list');
-            $this->set(compact('mealNames', 'recipes', 'users'));
+        if (!$this->MealPlan->exists($id)) {
+                throw new NotFoundException(__('Invalid meal plan'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+                if ($this->MealPlan->save($this->request->data)) {
+                        $this->Session->setFlash(__('The meal plan has been saved.'));
+                        return $this->redirect(array('action' => 'index'));
+                } else {
+                        $this->Session->setFlash(__('The meal plan could not be saved. Please, try again.'));
+                }
+        } else {
+                $options = array('conditions' => array('MealPlan.' . $this->MealPlan->primaryKey => $id));
+                $this->request->data = $this->MealPlan->find('first', $options);
+        }
+        $mealNames = $this->MealPlan->MealName->find('list');
+        $recipes = $this->MealPlan->Recipe->find('list');
+        $users = $this->MealPlan->User->find('list');
+        $this->set(compact('mealNames', 'recipes', 'users'));
     }
 
     /**
@@ -58,17 +64,17 @@ class MealPlansController extends AppController {
      * @return void
      */
     public function delete($id = null) {
-            $this->MealPlan->id = $id;
-            if (!$this->MealPlan->exists()) {
-                    throw new NotFoundException(__('Invalid meal plan'));
-            }
-            $this->request->onlyAllow('post', 'delete');
-            if ($this->MealPlan->delete()) {
-                    $this->Session->setFlash(__('The meal plan has been deleted.'));
-            } else {
-                    $this->Session->setFlash(__('The meal plan could not be deleted. Please, try again.'));
-            }
-            return $this->redirect(array('action' => 'index'));
+        $this->MealPlan->id = $id;
+        if (!$this->MealPlan->exists()) {
+                throw new NotFoundException(__('Invalid meal plan'));
+        }
+        $this->request->onlyAllow('post', 'delete');
+        if ($this->MealPlan->delete()) {
+                $this->Session->setFlash(__('The meal plan has been deleted.'));
+        } else {
+                $this->Session->setFlash(__('The meal plan could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
     }
 
 }
--- app/View/MealPlans/index.ctp
+++ app/View/MealPlans/index.ctp
@@ -1,13 +1,37 @@
+<script type="text/javascript">
+    $(function() {
+        $('#loadToday').click(function() {
+            ajaxGet('MealPlans/index/<?php echo date('m-d-Y');?>');
+        });
+    });
+</script>
 <div class="mealPlans index">
 	<h2><?php echo __('Meal Plan - Weekly'); ?></h2>
-        <br/>
-        <div id="weeklyContainer"/>
+        
+        <?php echo $this->Html->link('<', 
+                array('action' => 'index', $previousWeek[1].'-'.$previousWeek[0].'-'.$previousWeek[2]), 
+                array('class' => 'ajaxNavigation calendarNavigation')); ?>
+                    
+        <?php echo $startDate;?> - <?php echo $endDate;?>
+        
+        <?php echo $this->Html->link('>', 
+                array('action' => 'index', $nextWeek[1].'-'.$nextWeek[0].'-'.$nextWeek[2]), 
+                array('class' => 'ajaxNavigation calendarNavigation')); ?>
+        <button id="loadToday"><?php echo __('Today');?></button>
+        
+        <br/><br/>
+        <div id="weeklyContainer">
 	<?php foreach ($weekDays as $day): ?>
         <div class="dayHeader"><?php echo $day ?>
         </div>
         <?php endforeach; ?>
-        <?php for ($i=1; $i < 8; $i++) : ?>
-        <div class="dayContent <?php echo ($i == 7) ? "endOfRow" : "";?>"><a href="#"><?php echo $i;?></a></div>
+        <?php for ($i=0; $i < 7; $i++) : ?>
+        <div class="dayContent 
+            <?php echo ($i == 6) ? "endOfRow" : "";?> 
+            <?php echo ($weekList[$i][1] != $currentMonth) ? "nextMonth" : "";?>
+            <?php echo ($weekList[$i][1] == $realMonth && $weekList[$i][0] == $realDay && $weekList[$i][2] == $realYear ) ? "currentDay" : "";?>
+        ">
+            <a href="#"><?php echo $weekList[$i][0];?></a></div>
         <?php endfor; ?>
         </div>
         <div class="clear"></div>
