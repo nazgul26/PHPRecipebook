5ddc021e233db31ddbb21a51392f50b438091187
Todd Rogers <todd@windfox.net>
2014-12-31 22:37:48 -0500

    save of mealplan done. Not day specific though

--- app/Controller/MealPlansController.php
+++ app/Controller/MealPlansController.php
@@ -25,7 +25,17 @@ class MealPlansController extends AppController {
         $realDay = $this->MealPlan->realDay;
         $realMonth = $this->MealPlan->realMonth;
         $realYear = $this->MealPlan->realYear;
-        $this->set(compact('weekDays', 'weekList', 'currentMonth', 'startDate', 'endDate', 'nextWeek', 'previousWeek', 'realDay', 'realMonth', 'realYear'));
+        
+        $startDateQuery = $weekList[0][2] . "-" . $weekList[0][1] . "-" . $weekList[0][0];
+        $endDateQuery = $weekList[6][2] . "-" . $weekList[6][1] . "-" . $weekList[6][0];
+        $meals = $this->MealPlan->find('all', array(
+              'conditions' => array('MealPlan.mealday BETWEEN ? AND ?' => array($startDateQuery,$endDateQuery))
+        ));
+        
+        foreach ($meals as $item) {
+            $mealList[$item["MealPlan"]["mealday"]] = $item;
+        }
+        $this->set(compact('mealList', 'weekDays', 'weekList', 'currentMonth', 'startDate', 'endDate', 'nextWeek', 'previousWeek', 'realDay', 'realMonth', 'realYear'));
     }
 
     /**
@@ -36,24 +46,23 @@ class MealPlansController extends AppController {
      * @return void
      */
     public function edit($id = null) {
-        if (!$this->MealPlan->exists($id)) {
+        if ($id != null && !$this->MealPlan->exists($id)) {
                 throw new NotFoundException(__('Invalid meal plan'));
         }
         if ($this->request->is(array('post', 'put'))) {
-                if ($this->MealPlan->save($this->request->data)) {
-                        $this->Session->setFlash(__('The meal plan has been saved.'));
-                        return $this->redirect(array('action' => 'index'));
-                } else {
-                        $this->Session->setFlash(__('The meal plan could not be saved. Please, try again.'));
-                }
+            if ($this->MealPlan->save($this->request->data)) {
+                $this->Session->setFlash(__('The meal plan has been saved.'), "success");
+                return $this->redirect(array('action' => 'edit'));
+            } else {
+                $this->Session->setFlash(__('The meal plan could not be saved. Please, try again.'));
+            }
         } else {
-                $options = array('conditions' => array('MealPlan.' . $this->MealPlan->primaryKey => $id));
-                $this->request->data = $this->MealPlan->find('first', $options);
+            $options = array('conditions' => array('MealPlan.' . $this->MealPlan->primaryKey => $id));
+            $this->request->data = $this->MealPlan->find('first', $options);
         }
         $mealNames = $this->MealPlan->MealName->find('list');
         $recipes = $this->MealPlan->Recipe->find('list');
-        $users = $this->MealPlan->User->find('list');
-        $this->set(compact('mealNames', 'recipes', 'users'));
+        $this->set(compact('mealNames', 'recipes'));
     }
 
     /**
--- app/View/MealPlans/index.ctp
+++ app/View/MealPlans/index.ctp
@@ -7,7 +7,10 @@
 </script>
 <div class="mealPlans index">
 	<h2><?php echo __('Meal Plan - Weekly'); ?></h2>
-        
+<pre>
+<?php echo print_r($mealList);?>
+</pre>
+        <?php echo $this->element('sql_dump'); ?>
         <?php echo $this->Html->link('<', 
                 array('action' => 'index', $previousWeek[1].'-'.$previousWeek[0].'-'.$previousWeek[2]), 
                 array('class' => 'ajaxNavigation calendarNavigation')); ?>
@@ -22,7 +25,8 @@
         <br/><br/>
         <div id="weeklyContainer">
 	<?php foreach ($weekDays as $day): ?>
-        <div class="dayHeader"><?php echo $day ?>
+        <div class="dayHeader">
+            <?php echo $day;?>
         </div>
         <?php endforeach; ?>
         <?php for ($i=0; $i < 7; $i++) : ?>
@@ -31,9 +35,16 @@
             <?php echo ($weekList[$i][1] != $currentMonth) ? "nextMonth" : "";?>
             <?php echo ($weekList[$i][1] == $realMonth && $weekList[$i][0] == $realDay && $weekList[$i][2] == $realYear ) ? "currentDay" : "";?>
         ">
-            <a href="#"><?php echo $weekList[$i][0];?></a></div>
-        <?php endfor; ?>
+            <?php echo $this->Html->link($weekList[$i][0], array('action' => 'edit'), array('class' => 'ajaxLink', 'targetId' => 'editMealDialog'));?>
+            <br/>
+                <?php 
+                $dateIndex = $weekList[$i][2] . "-" . str_pad($weekList[$i][1], 2, "0",STR_PAD_LEFT) . "-" . str_pad($weekList[$i][0], 2, "0",STR_PAD_LEFT);
+                if (isset($mealList[$dateIndex])) {
+                echo $mealList[$dateIndex]["Recipe"]["name"];
+            } ?>
         </div>
+        <?php endfor; ?>
+        
         <div class="clear"></div>
 </div>
 
