bfabd7b310ecd9935a7cecae57f3539632aa0c64
Todd Rogers <todd@windfox.net>
2015-01-10 21:51:20 -0500

    starting to use sass and compass for CSS.

---
+++ app/sass/config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+#Folder settings
+relative_assets = true #because we're not working from the root
+css_dir = "../webroot/css" #where the CSS will saved
+sass_dir = "src" #where our .scss files are
+images_dir = "../webroot/images" #the folder with your images
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded # After dev :compressed
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+# Obviously
+preferred_syntax = :scss
