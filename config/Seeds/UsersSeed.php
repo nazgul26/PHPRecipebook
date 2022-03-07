d8e7ff0fa29107da76237b01ca3a5b5d713160a1
Todd Rogers <toddrogers3286@gmail.com>
2022-03-07 16:13:40 -0500

    Adding Back in SQL Files

---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,45 @@
+<?php
+use Migrations\AbstractSeed;
+use Cake\Auth\DefaultPasswordHasher;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'username' => 'admin',
+                'password' => (new DefaultPasswordHasher)->hash('passwd'),
+                'name' => 'Administrator',
+                'access_level' => '90',
+                'language' => 'en',
+                'country' => 'us',
+                'created' => '2003-03-09 00:00:00',
+                'last_login' => '2014-06-17 00:00:00',
+                'email' => 'admin@phprecipebook',
+                'id' => '1',
+                'modified' => '2015-02-22 02:34:56',
+                'reset_token' => NULL,
+                'locked' => '0',
+                'reset_time' => '2015-01-19 01:49:51',
+                'meal_plan_start_day' => '6',
+            ],
+        ];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+}
