a2f2815bf8654e97e3a271f82ce426594e6cf4aa
Luca Pireddu <pireddu@crs4.it>
2026-02-19 23:25:33 +0100

    Start revising container image and docker compose

--- docker/web-start.sh
+++ docker/web-start.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+set -o errexit
+set -o nounset
+
 # Update Schema if needed
+echo "Running migrations" >&2
 /var/www/html/bin/cake migrations migrate
 
-CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
-if [ ! -e /var/www/html/$CONTAINER_FIRST_STARTUP ]; then
-    touch /var/www/html/$CONTAINER_FIRST_STARTUP
-    # Only run seed once
+# Run seed if requested
+if [ "${PHPRB_INSERT_SEEDS:-}" = "true" ]; then
+    echo "Seeding database" >&2
     /var/www/html/bin/cake migrations seed
 fi
 
-apache2ctl -DFOREGROUND
+echo "Starting apache2" >&2
+exec apache2ctl -DFOREGROUND
