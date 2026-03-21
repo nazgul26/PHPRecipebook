7c727a720e31291816461d951d4b74cb6329455f
Luca Pireddu <pireddu@crs4.it>
2026-03-21 17:09:31 +0100

    Add option to run migrations from docker entrypoint
    With this addition one should be able to easily use the container
    without the compose (or with a different compose from the one provided).

--- docker/web-start.sh
+++ docker/web-start.sh
@@ -3,6 +3,12 @@
 set -o errexit
 set -o nounset
 
+# Run migrations, if requested
+if [ "${PHPRB_RUN_MIGRATIONS:-}" = "true" ]; then
+    echo "Running migrations" >&2
+    /var/www/html/bin/cake migrations migrate
+fi
+
 # Run seed if requested. Unless using a version of Cake that
 # avoics duplicate seeding, it's better to do this manually.
 if [ "${PHPRB_INSERT_SEEDS:-}" = "true" ]; then
